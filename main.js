4078f3db7386ae973de68b5342385c97ce233594
Saba Mirshahi <sabamirri@gmail.com>
2022-09-13 20:13:46 +0430

    code for buttons of cart added

---
+++ main.js
@@ -0,0 +1,36 @@
+var removeCartItemButtons = document.getElementsByClassName('btn-danger');
+
+
+
+
+
+for (var i = 0; i < removeCartItemButtons.length; i++) {
+   var button = removeCartItemButtons[i];
+   button.addEventListener("click", function(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove()
+    
+    updateCartTotal()
+    
+   })
+    
+}
+
+function updateCartTotal() {
+var cartItemContainer = document.getElementsByClassName('cart-items')[0];
+var cartRow = cartItemContainer.getElementsByClassName('cart-row');
+var total = 0;
+for (var i = 0; i < cartRow.length; i++) {
+    var cartRow = cartRow[i];
+    var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+    var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+    var price = parseFloat(priceElement.innerText.replace('$', ''));
+    var quantity = quantityElement.value;
+    total = total + (price * quantity);   
+}
+
+document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total;
+
+}
+
+
